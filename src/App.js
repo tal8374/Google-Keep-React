e7da19a0e476fa198e254cab7a7b1bace8ce64e8
tal8374 <anatoly.i@reflectiz.com>
2020-07-18 11:12:08 +0300

    Moving everything to main page before using router

--- src/App.js
+++ src/App.js
@@ -1,56 +1,35 @@
 import React, { Component } from 'react';
 
 import { Helmet } from "react-helmet";
+import { Router } from "@reach/router";
 
 import { CssBaseline } from "@material-ui/core";
 
-import Container from "@material-ui/core/Container";
-import Box from "@material-ui/core/Box";
+import { UserProvider } from './shared/contexts/userProvider';
+import { AppThemeProvider } from './shared/contexts/themeContext';
 
-import AppNavBar from './parts/AppNavbar/AppNavbar'
-import AppSideBar from './parts/AppNavbar/AppSideBar/AppSideBar'
-import NotesArea from './parts/NotesArea/NotesArea'
-
-import { UserProvider } from './shared/contexts/userProvider'
-import { AppThemeProvider } from './shared/contexts/themeContext'
-import { ListViewProvider } from './shared/contexts/listViewProvider'
-import { MenuBarProvider } from './shared/contexts/menuBarProvider'
-import { LabelsProvider, TodosProvider, SelectedLabelProvider, NoteInEditModeProvider } from './shared/contexts/todosProvider'
+import Main from './pages/main';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+
         <Helmet>
           <meta
             name="viewport"
             content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
           />
         </Helmet>
+        {/* <Router> */}
         <AppThemeProvider>
-          <ListViewProvider>
-            <MenuBarProvider>
-              <LabelsProvider>
-                <TodosProvider>
-                  <SelectedLabelProvider>
-                    <NoteInEditModeProvider>
-                      <UserProvider>
-                        <CssBaseline />
-                        <AppNavBar />
-                        <AppSideBar />
-                        <Container maxWidth={false}>
-                          <Box mt={8}>
-                            <NotesArea />
-                          </Box>
-                        </Container>
-                      </UserProvider>
-                    </NoteInEditModeProvider>
-                  </SelectedLabelProvider>
-                </TodosProvider>
-              </LabelsProvider>
-            </MenuBarProvider>
-          </ListViewProvider>
+          <UserProvider>
+            <CssBaseline />
+
+            <Main />
+          </UserProvider>
         </AppThemeProvider>
+        {/* </Router> */}
       </React.Fragment>
     );
   }
---
+++ src/pages/main.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { ListViewProvider } from '../shared/contexts/listViewProvider';
+import { MenuBarProvider } from '../shared/contexts/menuBarProvider';
+import { LabelsProvider, TodosProvider, SelectedLabelProvider, NoteInEditModeProvider } from '../shared/contexts/todosProvider';
+
+import AppNavBar from '../parts/AppNavbar/AppNavbar';
+import AppSideBar from '../parts/AppNavbar/AppSideBar/AppSideBar';
+import NotesArea from '../parts/NotesArea/NotesArea';
+
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+
+const Main = () => {
+
+    return (
+        <ListViewProvider>
+        <MenuBarProvider>
+          <LabelsProvider>
+            <TodosProvider>
+              <SelectedLabelProvider>
+                <NoteInEditModeProvider>
+                  <AppNavBar />
+                  <AppSideBar />
+                  <Container maxWidth={false}>
+                    <Box mt={8}>
+                      <NotesArea />
+                    </Box>
+                  </Container>
+                </NoteInEditModeProvider>
+              </SelectedLabelProvider>
+            </TodosProvider>
+          </LabelsProvider>
+        </MenuBarProvider>
+      </ListViewProvider>
+    );
+
+}
+
+export default Main;
